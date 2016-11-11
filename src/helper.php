e2e80182125c155a74850be9e4ecc2de7cab6601
hiword <28737164@qq.com>
2016-11-11 15:18:26 +0800

    queue test

--- src/helper.php
+++ src/helper.php
@@ -5,5 +5,6 @@ if (!function_exists('behavior_log'))
     function behavior_log(string $remark,\Illuminate\Database\Eloquent\Model $model)
     {
         app(\CrCms\Log\Repositories\BehaviorRepository::class)->create($model,$remark);
+//        dispatch((new \CrCms\Log\Jobs\BehaviorLogJob($remark,$model))->onQueue('log'));
     }
 }

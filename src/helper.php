adb87017dbb67767c23877152d0c3cb71f41fea9
hiword <28737164@qq.com>
2016-11-14 19:20:08 +0800

    参数调换，增加默认值

--- src/helper.php
+++ src/helper.php
@@ -2,7 +2,7 @@
 
 if (!function_exists('behavior_log'))
 {
-    function behavior_log(string $logType,string $remark,\Illuminate\Database\Eloquent\Model $model)
+    function behavior_log(string $remark,string $logType = \CrCms\Log\Repositories\BehaviorRepository::LOG_TYPE_INFO,\Illuminate\Database\Eloquent\Model $model)
     {
         app(\CrCms\Log\Repositories\BehaviorRepository::class)->create($model,$logType,$remark);
 //        dispatch((new \CrCms\Log\Jobs\BehaviorLogJob($remark,$model))->onQueue('log'));

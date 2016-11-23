d7053df89ba74a0fee8991cae713287309fa93a5
hiword <28737164@qq.com>
2016-11-23 11:13:45 +0800

    debug

--- config/log.php
+++ config/log.php
@@ -1,15 +1,13 @@
 <?php
 return [
-    'log_model'=>[
 
+    'log_model'=>[
         \CrCms\Log\Observers\BehaviorObserver::class=>[
             //Namespace\Model
         ]
     ],
 
-    'user_session_key'=>[
-
-    ],
+    'user_session_key'=>[],
 
     //'debug','info','warning','danger','error'
     'log_level'=>'info',
--- src/helper.php
+++ src/helper.php
@@ -5,6 +5,5 @@ if (!function_exists('behavior_log'))
     function behavior_log(string $remark,\Illuminate\Database\Eloquent\Model $model,string $logType = \CrCms\Log\Repositories\BehaviorRepository::LOG_TYPE_INFO)
     {
         app(\CrCms\Log\Repositories\BehaviorRepository::class)->create($model,$logType,$remark);
-//        dispatch((new \CrCms\Log\Jobs\BehaviorLogJob($remark,$model))->onQueue('log'));
     }
 }

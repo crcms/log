f0b4de8facd7bb19074d175261ecc5417889aa66
simon <crcms@crcms.cn>
2017-03-10 17:03:32 +0800

    format

--- src/helper.php
+++ src/helper.php
@@ -4,10 +4,11 @@ if (!function_exists('behavior_log'))
 {
     function behavior_log(string $remark,$model = null,string $status = '',string $logType = 'info')
     {
-        app(CrCms\Log\Services\BehaviorLogService::class)->setLogType($logType)
-            ->setLogModel($model)
-            ->setLogRemark($remark)
-            ->setLogStatus($status)
-            ->save();
+        app(CrCms\Log\Services\BehaviorLogService::class)
+        ->setLogType($logType)
+        ->setLogModel($model)
+        ->setLogRemark($remark)
+        ->setLogStatus($status)
+        ->save();
     }
 }
